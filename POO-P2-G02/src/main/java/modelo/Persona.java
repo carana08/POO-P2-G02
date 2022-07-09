8cc9f75dd4eb9e5ccc133dc685f00d47b8262741
carana08 <carana@espol.edu.ec>
2022-07-09 14:24:54 -0500

    Actualizando

--- POO-P2-G02/src/main/java/modelo/Persona.java
+++ POO-P2-G02/src/main/java/modelo/Persona.java
@@ -9,5 +9,59 @@ package modelo;
  * @author César
  */
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String telefono;
+    private String email;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Persona(String nombre, String apellido, String cedula, String telefono, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.email = email;
+    }
+    
     
 }
