8803face19ad536b35e04aca7f8ca5434cdc8c69
kdmirand <kdmirand@espol.edu.ec>
2022-07-11 19:26:20 -0500

    Actualizando Empleado

---
+++ POO-P2-G02/src/main/java/modelo/Empleado.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.util.ArrayList;
+/**
+ *
+ * @author Karen
+ */
+public class Empleado extends Persona{
+    private String estado;
+    private ArrayList<Servicio> servicios;
+    
+    //Constructor
+    public Empleado(String nombre, String apellido, String cedula, String telefono, String email, String estado, ArrayList<Servicio> servicios){
+        super(nombre, apellido, cedula, telefono, email);
+        this.estado = estado;
+        this.servicios = servicios;
+    }
+    public void agregarEmpleado()
+}
