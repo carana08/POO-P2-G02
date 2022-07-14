a11520a133b7d3e1d5792fc32657bd73da0524d2
kdmirand <kdmirand@espol.edu.ec>
2022-07-13 19:03:14 -0500

    Actualizando, queda pendiente Atencion

--- POO-P2-G02/src/main/java/modelo/Atencion.java
+++ POO-P2-G02/src/main/java/modelo/Atencion.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
+import java.util.ArrayList;
 /**
  *
  * @author Karen
  */
 public class Atencion {
+    private Cita cita;
+    private ArrayList<Atencion> atenciones = new ArrayList<> ();
     
+    public Atencion(Cita cita, ArrayList<Atencion> atenciones){
+        this.cita = cita;
+        this.atenciones = atenciones;
+    }
+    
+    public void registrarAtencion(){
+        Empleado e = cita.getEmpleado();
+    }
+    public void consultarAtencion(){
+        
+    }
 }
