f119aa247a842c80aa97f24dd41c25eff99518ec
César Arana <95942498+carana08@users.noreply.github.com>
2022-08-09 17:31:45 -0500

    Delete Parcial2/src/main directory

--- Parcial2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.parcial2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    //requires java.base;
-
-    opens com.mycompany.parcial2 to javafx.fxml;
-    exports javafxbase;
-}
